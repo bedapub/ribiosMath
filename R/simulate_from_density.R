e226e96f9d2a4a82c143a7419fe450608eccf6ca
Jitao David Zhang <jitao_david.zhang@roche.com>
2023-01-27 15:22:56 +0100

    add new function simulate_from_density by Iakov

---
+++ R/simulate_from_density.R
@@ -0,0 +1,16 @@
+#' Simulate from density
+#' Compared with bootstrapping, the results do not reveal input values, and the
+#' empirical distribution can be more smooth. The function assumes that the data
+#' is normally distributed.
+#' @param vec Numeric vector
+#' @param N Integer, number of simulated instances
+#' @author Iakov Davydov
+#' @examples
+#' my_vec <- c(23, 27, 26, 24, 25)
+#' simulate_from_density(my_vec, 10)
+#' @importFrom stats density rnorm
+#' @export
+simulate_from_density <- function(vec, N = 1e5) {
+  fit <- density(vec, bw="SJ")
+  rnorm(N, sample(vec, size = N, replace = TRUE), fit$bw)
+}
